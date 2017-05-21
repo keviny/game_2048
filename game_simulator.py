fe744a57ea47f535d388a279e642fdd03279b67c
kevin <gforrest.yang@gmail.com>
2017-05-21 19:54:33 +0800

    first simulation code

--- game_simulator.py
+++ game_simulator.py
@@ -3,15 +3,16 @@ import game
 import logging
 import numpy as np
 import game_model
-import sys
+import random
+import time
 import tensorflow as tf
 
 parser = argparse.ArgumentParser()
 parser.add_argument(
-    '-d', '--debug',
-    help="Print lots of debugging statements",
-    action="store_const", dest="loglevel", const=logging.DEBUG,
-    default=logging.INFO)
+  '-d', '--debug',
+  help="Print lots of debugging statements",
+  action="store_const", dest="loglevel", const=logging.INFO,
+  default=logging.INFO)
 
 parser.add_argument('--use_ai', help="choose to use ai", action="store_true", default=False)
 
@@ -21,100 +22,69 @@ parser.add_argument('--log_name', default='game.log')
 
 parser.add_argument('--exp_name', help="choose to use ai", default='d1')
 
-
 args = parser.parse_args()
 
 logging.basicConfig(filename=args.log_name, level=args.loglevel)
 
 
 class GameSimulator(object):
-  def __init__(self, model_path, use_ai=True, exp_name="d1"):
+  def __init__(self, game, model_path, use_ai=True, exp_name="d1"):
+    self.game = game
     self.model = game_model.GameModel("train", 1)
     self.target_board_q = self.model.get_internal_variable().get("action_q")
     self.sess = tf.Session()
-    self.sess.run(tf.global_variables_initializer())
-    print self.sess.run(self.model.get_params_dict().get("model_conv1_weights"))
-    self.saver = tf.train.import_meta_graph(model_path + "%s/model.meta" % exp_name)
-    self.saver.restore(self.sess, model_path + "%s/model.ckpt" % exp_name)
     self._use_ai = use_ai
-    print self.sess.run(self.model.get_params_dict().get("model_conv1_weights"))
 
-  def eval(self, eval_board):
-      """
-      This function calcuates the possbile value of next step and
-      and returns max value of the sum of q value and incrmental score.
-      """
-      internal_game = game.Game()
-      internal_game.load_board(eval_board)
-      internal_game.random_gen()
-      raw_directions = internal_game.get_valid_directions(False)
-      board_score_dict = internal_game.get_next_boards(raw_directions)
-      valid_directions = filter(lambda x: x != -1, raw_directions)
-      next_total_score = []
-      for direction in [game.Direction.left,
-                        game.Direction.right,
-                        game.Direction.up,
-                        game.Direction.down]:
-          if direction not in valid_directions:
-              # If the direction is not valid, add a negative value in it.
-              next_total_score.append(-sys.maxint)
-              continue
-          move_board, move_score = board_score_dict[direction]
-          max_board = max(move_board)
-          if self._use_ai == False:
-            next_total_score.append(move_score)
-          else:
-            board_q_value = self.sess.run(
-                [self.target_board_q],
-                feed_dict={
-                  self.model.get_pl_dict().get("c_b"): np.array(move_board).reshape(1, 4, 4, 1).astype(float),
-                  self.model.get_pl_dict().get("c_m"): np.array(max_board).reshape(1, 1).astype(float),
-                  #self.model.get_pl_dict().get("c_r"): np.array([1.0]).reshape(1, 1).astype(float),
-                  #self.model.get_pl_dict().get("c_a"): np.array([1.0, 1.0, 1.0, 1.0]).reshape(1, 4).astype(float),
-                  self.model.get_pl_dict().get("dropout_keep_prob"): 1.0
-                  })
-            # Adds the q value of next step board and incrmental score
-            next_total_score.append(board_q_value[0][0][0] + move_score)
-      return max(next_total_score), next_total_score
+    saver = tf.train.Saver()
+    ckpt = tf.train.get_checkpoint_state(model_path + exp_name)
+    saver.restore(self.sess, ckpt.model_checkpoint_path)
 
-  def move(self, current_board):
-      internal_game = game.Game()
-      internal_game.load_board(current_board)
-      raw_directions = internal_game.get_valid_directions(False)
-      board_score_dict = internal_game.get_next_boards(raw_directions)
-      logging.debug(board_score_dict)
-      valid_directions = filter(lambda x: x != -1, raw_directions)
-      # The score array for the first layer of movement.
-      next_total_score = []
-      # The score array fro the second layer of movement.
-      next_move_score = []
-      for direction in [game.Direction.left,
-                        game.Direction.right,
-                        game.Direction.up,
-                        game.Direction.down]:
-          logging.debug(direction)
-          if direction not in valid_directions:
-              # If the direction is not valid, add a negative value in it.
-              next_total_score.append(-sys.maxint)
-              continue
-          move_board, move_score = board_score_dict[direction]
-          next_best_score, next_score_array = self.eval(move_board)
-          next_move_score.append({direction:next_score_array})
-          next_total_score.append(next_best_score + move_score)
-      target_move = np.argmax(np.array(next_total_score))
-      return target_move, next_total_score, next_move_score
+  def eval(self):
+    board_q_value = self.sess.run(
+          [self.target_board_q],
+          feed_dict={
+            self.model.get_pl_dict().get("c_b"): np.array(self.game.get_board()).reshape(1, 4, 4, 1).astype(float),
+            self.model.get_pl_dict().get("c_m"): np.array(max(self.game.get_board())).reshape(1, 1).astype(float),
+            self.model.get_pl_dict().get("dropout_keep_prob"): 1.0
+          })
+    return board_q_value[0][0]
+
+  def get_game(self):
+    return self.game;
+
+  def move(self):
+    raw_directions = self.game.get_valid_directions(False)
+    board_score_dict = self.game.get_next_boards(raw_directions)
+    if self._use_ai:
+      next_score_list = self.eval()
+    else:
+      next_score_list = map(lambda k: board_score_dict[k][1], board_score_dict)[0:4]
+    # max score must be in the set of valid direction.
+    max_score = max(map(lambda i: next_score_list[i], self.game.get_valid_directions()))
+    max_direction = []
+    for i in xrange(0, 4):
+      # The direction must be valid direction.
+      if i in self.game.get_valid_directions() and abs(next_score_list[i] - max_score) < 0.0001:
+        max_direction.append(i)
+    self.game.move(random.choice(max_direction))
+
 
 if __name__ == "__main__":
-  simulator = GameSimulator(args.model_path, args.use_ai, args.exp_name)
-  for x in xrange(100):
-    target_game = game.Game()
-    while len(target_game.get_valid_directions()) > 0:
-      if args.loglevel==logging.DEBUG:
-        target_game.display()
-      move_direction, first_layer_score, second_layer_score = simulator.move(target_game.get_board())
-      target_game.move(move_direction)
-      logging.debug("%s %s" % (first_layer_score, second_layer_score))
-    log_content = "%d:%f" % (target_game.get_action_counter(), target_game.get_score())
-    logging.info("Final result: %05d %s" % (x, log_content))
+  simulator = GameSimulator(game.Game(), args.model_path, args.use_ai, args.exp_name)
+  step_list = []    # For stat
+  score_list = []   # For stat
+  total_simulation_counter = 1000
+  for x in xrange(total_simulation_counter):
+    simulator.get_game().reset()
+    while len(simulator.get_game().get_valid_directions()) > 0:
+      simulator.move()
+    simulator.get_game().display()
+    log_content = "step=%d\tmax=%d\tscore=%d" % (simulator.get_game().get_action_counter(),
+                                  max(simulator.get_game().get_board()),
+                                  simulator.get_game().get_score())
+    logging.info("Record id:%05d info: %s" % (x, log_content))
     print "Final result: %05d %s" % (x, log_content)
-    target_game.display()
+    step_list.append(simulator.get_game().get_action_counter())
+    score_list.append(simulator.get_game().get_score())
+  print "Average step %d, Average score %d" % (sum(step_list) / total_simulation_counter,
+                                               sum(score_list) / total_simulation_counter)
